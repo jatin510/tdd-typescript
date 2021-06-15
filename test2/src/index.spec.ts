b31087f58bc35b1ecf7beda9dffe0e55132a660f
jatin_510 <jagdish@quor.com>
2021-06-15 17:57:02 +0530

    added return types of the function

--- test2/src/index.spec.ts
+++ test2/src/index.spec.ts
@@ -10,11 +10,11 @@ class Stack {
     return this.items[this.top];
   }
 
-  pop(): any {
+  pop(): void {
     this.top === -1 ? -1 : (this.top -= 1);
   }
 
-  push(value): any {
+  push(value: number): void {
     this.top += 1;
     this.items[this.top] = value;
   }
